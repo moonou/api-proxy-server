968c69d97681578671822e1847c6d25247f483f4
moonou <moonoulong@gmail.com>
2016-10-31 18:04:06 +0800

    Mod: mod response to stream

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 'use strict'
 const app = require('koa')()
 const request = require('request')
+const stream = require('stream')
 
 app.use(function *(){
   if (this.query.target) {
-    let result = yield new Promise((resolve, reject) => {
-      request({
-        url: this.query.target,
-        headers: {
-          'User-Agent': 'Awesome-Octocat-App'
-        }
-      }, (err, response, body) => {
-        if (err) throw err
-        resolve(body)
-      })
+    this.body = request({
+      url: this.query.target,
+      headers: {
+        'User-Agent': 'Awesome-Octocat-App'
+      }
     })
-    this.body = result
     this.set('Access-Control-Allow-origin', '*')
   } else {
     this.body = 'miss query'
